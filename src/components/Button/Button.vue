972290ba2ef3c772e3ae8005412822f4adf93b21
xiaoli <2585960098@qq.com>
2023-07-28 01:08:17 +0800

    init

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,23 @@
+<template>
+  <button
+    class="st-button"
+    :class="{
+      [`st-button--${type}`]: type,
+      [`st-button--${size}`]: size,
+      'is-plain': plain,
+      'is-round': round,
+      'is-circle': circle,
+      'is-disabled': disabled
+    }"
+    :disabled="disabled"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts" setup>
+import type { ButtonProps } from './types'
+defineProps<ButtonProps>()
+</script>
+
+<style scoped></style>
