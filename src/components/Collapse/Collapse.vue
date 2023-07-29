068b02fff642a09e8587c95aca9ee94d39cbaace
xiaoli <2585960098@qq.com>
2023-07-29 15:21:20 +0800

    feat: Collapse组件动画

--- src/components/Collapse/Collapse.vue
+++ src/components/Collapse/Collapse.vue
@@ -22,7 +22,13 @@ if (props.accordion && activeNames.value.length > 1) {
 }
 const handleItemClick = (item: NameType) => {
   if (props.accordion) {
-    activeNames.value = [activeNames.value[0] === item ? '' : item]
+    if (activeNames.value[0] === item) {
+      activeNames.value.pop()
+      activeNames.value.push('')
+    } else {
+      activeNames.value.pop()
+      activeNames.value.push(item)
+    }
   } else {
     const index = activeNames.value.indexOf(item)
     if (index > -1) {
--- src/components/Collapse/CollapseItem.vue
+++ src/components/Collapse/CollapseItem.vue
@@ -5,12 +5,21 @@
       'is-disabled': disabled
     }"
   >
-    <div class="st-collapse-item__header" :id="`item-header-${name}`" @click="handleClick">
+    <div
+      class="st-collapse-item__header"
+      :class="{ 'is-disabled': disabled, 'is-active': isActive }"
+      :id="`item-header-${name}`"
+      @click="handleClick"
+    >
       <slot name="title">{{ title }}</slot>
     </div>
-    <div class="st-collapse-item__content" :id="`item-content-${name}`" v-show="isActive">
-      <slot />
-    </div>
+    <Transition name="slide" v-on="transitionEvents">
+      <div class="st-collapse-item__wrapper" v-show="isActive">
+        <div class="st-collapse-item__content" :id="`item-content-${name}`">
+          <slot />
+        </div>
+      </div>
+    </Transition>
   </div>
 </template>
 
@@ -26,6 +35,30 @@ const isActive = computed(() => collapseContext?.activeNames.value.includes(prop
 const handleClick = () => {
   if (props.disabled) return
   collapseContext?.handleItemClick(props.name)
+}
+const transitionEvents: Record<string, (el: HTMLElement) => void> = {
+  beforeEnter(el) {
+    el.style.height = '0px'
+    el.style.overflow = 'hidden'
+  },
+  enter(el) {
+    el.style.height = `${el.scrollHeight}px`
+  },
+  afterEnter(el) {
+    el.style.height = ''
+    el.style.overflow = ''
+  },
+  beforeLeave(el) {
+    el.style.height = `${el.scrollHeight}px`
+    el.style.overflow = 'hidden'
+  },
+  leave(el) {
+    el.style.height = '0px'
+  },
+  afterLeave(el) {
+    el.style.height = ''
+    el.style.overflow = ''
+  }
 }
 </script>
 
