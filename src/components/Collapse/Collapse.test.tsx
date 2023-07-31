9161568dc11c3edea3c5574b11a326448524b11e
xiaoli <2585960098@qq.com>
2023-08-01 00:17:43 +0800

    chore: 增加手风琴测试用例

--- src/components/Collapse/Collapse.test.tsx
+++ src/components/Collapse/Collapse.test.tsx
@@ -47,6 +47,7 @@ describe('Collapse.vue', () => {
     secondContent = contents[1]
     disableContent = contents[2]
   })
+
   test('测试基础结构及对应文本', () => {
     // 长度
     expect(headers).toHaveLength(3)
@@ -58,6 +59,7 @@ describe('Collapse.vue', () => {
     expect(secondContent.isVisible()).toBeFalsy()
     expect(firstContent.text()).toBe('Content a')
   })
+
   test('点击标题展开/关闭内容', async () => {
     // 行为
     await firstHeader.trigger('click')
@@ -65,11 +67,13 @@ describe('Collapse.vue', () => {
     await secondHeader.trigger('click')
     expect(secondHeader.isVisible()).toBeTruthy()
   })
+
   test('发送正确的事件', () => {
     expect(onChange).toHaveBeenCalledTimes(2)
     expect(onChange).toHaveBeenCalledWith([])
     expect(onChange).toHaveBeenLastCalledWith(['b'])
   })
+
   test('disabled的内容应该没有反应', async () => {
     // disabled
     onChange.mockClear()
@@ -77,5 +81,48 @@ describe('Collapse.vue', () => {
     await disableHeader.trigger('click')
     expect(disableContent.isVisible()).toBeFalsy()
     expect(onChange).not.toHaveBeenCalled()
+  })
+  
+  test('accordion手风琴', async () => {
+    wrapper = mount(
+      () => {
+        return (
+          <Collapse accordion modelValue={['a']} onChange={onChange}>
+            <CollapseItem name="a" title="title a">
+              Content a
+            </CollapseItem>
+            <CollapseItem name="b" title="title b">
+              Content b
+            </CollapseItem>
+          </Collapse>
+        )
+      },
+      {
+        global: {
+          stubs: ['Icon']
+        },
+        attachTo: document.body
+      }
+    )
+    onChange.mockClear()
+    headers = wrapper.findAll('.st-collapse-item__header')
+    contents = wrapper.findAll('.st-collapse-item__content')
+    firstHeader = headers[0]
+    secondHeader = headers[1]
+    firstContent = contents[0]
+    secondContent = contents[1]
+    expect(headers).toHaveLength(2)
+    expect(contents).toHaveLength(2)
+    expect(firstContent.isVisible()).toBeTruthy()
+    expect(secondContent.isVisible()).toBeFalsy()
+    expect(disableContent.isVisible()).toBeFalsy()
+    await secondHeader.trigger('click')
+    expect(firstContent.isVisible()).toBeFalsy()
+    expect(secondContent.isVisible()).toBeTruthy()
+    expect(onChange).toHaveBeenCalledWith(['b'])
+    await firstHeader.trigger('click')
+    expect(onChange).toHaveBeenLastCalledWith(['a'])
+    expect(firstContent.isVisible()).toBeTruthy()
+    expect(secondContent.isVisible()).toBeFalsy()
   })
 })
