e944e74d516b7076cfaf44e780c75349e96f06fa
xiaoli <2585960098@qq.com>
2023-10-01 17:46:39 +0800

    feat: FormItem的validate

--- src/components/Form/FormItem.vue
+++ src/components/Form/FormItem.vue
@@ -1,23 +1,47 @@
 <template>
-  <div class="st-form-item">
+  <div
+    class="st-form-item"
+    :class="{
+      'is-error': validateStatus.state === 'error',
+      'is-success': validateStatus.state === 'success',
+      'is-loading': validateStatus.state === 'loading'
+    }"
+  >
     <label for="" class="st-form-item__label">
       <slot name="label" :label="label">{{ label }}</slot>
     </label>
     <div class="st-form-item__content">
       <slot />
+      <div
+        class="st-form-item__error-msg"
+        v-if="validateStatus.state === 'error'"
+      >
+        {{ validateStatus.errorMsg }}
+      </div>
     </div>
+    <button @click.prevent="validate">Validate</button>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { inject, computed } from 'vue'
+import { inject, computed, reactive } from 'vue'
 import { isNil } from 'lodash-es'
-import { type FormItemProps, formContextKey } from './types'
+import {
+  type FormItemProps,
+  formContextKey,
+  FormValidateFailure
+} from './types'
+import Schema from 'async-validator'
 defineOptions({
   name: 'StFormItem'
 })
 const props = defineProps<FormItemProps>()
 const formContext = inject(formContextKey)
+const validateStatus = reactive({
+  state: 'init',
+  errorMsg: '',
+  loading: false
+})
 const innerValue = computed(() => {
   const model = formContext?.model
   if (model && props.prop && !isNil(model[props.prop])) {
@@ -34,6 +58,30 @@ const itemRules = computed(() => {
     return []
   }
 })
+const validate = () => {
+  const modelName = props.prop
+  if (modelName) {
+    const validator = new Schema({
+      [modelName]: itemRules.value
+    })
+    validateStatus.loading = true
+    validator
+      .validate({ [modelName]: innerValue.value })
+      .then(() => {
+        validateStatus.state = 'success'
+      })
+      .catch((e: FormValidateFailure) => {
+        const { errors } = e
+        validateStatus.state = 'error'
+        validateStatus.errorMsg =
+          errors && errors.length ? errors[0].message || '' : ''
+        console.log(e.errors)
+      })
+      .finally(() => {
+        validateStatus.loading = false
+      })
+  }
+}
 </script>
 
 <style></style>
--- src/components/Form/types.ts
+++ src/components/Form/types.ts
@@ -1,16 +1,28 @@
 import type { InjectionKey } from 'vue'
+import type {
+  RuleItem,
+  ValidateError,
+  ValidateFieldsError
+} from 'async-validator'
 
 export interface FormItemProps {
   label?: string
   prop?: string
 }
 
+export type FormRules = Record<string, RuleItem[]>
+
 export interface FormProps {
-  model: Record<string, any>
-  rules: Record<string, any>
+  model: FormRules
+  rules: FormRules
 }
 
 export interface FormContext extends FormProps {}
+
+export interface FormValidateFailure {
+  errors: ValidateError[] | null
+  fields: ValidateFieldsError
+}
 
 export const formContextKey: InjectionKey<FormContext> =
   Symbol('formContextKey')
